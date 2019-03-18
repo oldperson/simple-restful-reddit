6777eafa8f422381e5e9acd647001f6a639459b8
chris.hung <chris.hung@gmail.com>
2019-03-18 22:23:52 +0800

    Add postRepository and its tests

---
+++ repositories/postRepository.js
@@ -0,0 +1,40 @@
+const GenericRepository = require('./genericRepository');
+const { Post } = require('../orm/models');
+
+/**
+ * @class Construct a post repository
+ * @param {Object} postModel Set up model to access resources.
+ */
+class PostRepository extends GenericRepository {
+  constructor(postModel) {
+    super(postModel || Post);
+  }
+
+  /**
+   * Create a post under in given community.
+   * @param {string} communityName
+   * @param {object} post
+   * @returns {Promise<object>} { postId }
+   */
+  createUnder(communityName, post) {
+    const { sequelize } = this.sequelizeModel;
+    const now = new Date().toISOString();
+    const replacements = Object.assign({
+      communityName,
+      createdAt: now,
+      updatedAt: now,
+    }, post);
+    const sql = `INSERT INTO 
+                   Post (communityId, title, content, authorId, createdAt, updatedAt)
+                 SELECT communityId, :title, :content, :authorId, :createdAt, updatedAt
+                   FROM  Community WHERE communityName = :communityName;`;
+
+    return sequelize.query(sql, {
+      replacements,
+      type: sequelize.QueryTypes.INSERT,
+    }).then(results => ({ postId: results[0] }));
+  }
+}
+
+module.exports = PostRepository;
+module.exports.instance = new PostRepository();
---
+++ test/integretion/repositories/postRepository.test.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+const { sequelize, User, Community } = require('../../../orm/models');
+const postRepository = require('../../../repositories/postRepository').instance;
+
+const defaultUser = {
+  userId: 1,
+  userName: 'test',
+  passwordHash: 'aaaaaa',
+  email: 'ggg@gmail.com',
+};
+const defaultCommunity = {
+  communityId: 1,
+  communityName: 'comForTest',
+};
+
+describe('postRepository', () => {
+  before('Set up user and community date',
+    () => sequelize.sync({ force: true })
+      .then(() => User.create(defaultUser))
+      .then(() => Community.create(defaultCommunity)));
+  afterEach(() => postRepository.sequelizeModel.truncate({ truncate: true }));
+
+  describe('creaeteUnder', () => {
+    it('should work when community and post data are provided', () => {
+      const post = {
+        title: 'new post',
+        content: 'hello',
+        authorId: defaultUser.userId,
+      };
+      return postRepository.createUnder(defaultCommunity.communityName, post)
+        .then((result) => {
+          expect(result.postId).to.exist;
+        });
+    });
+  });
+});
