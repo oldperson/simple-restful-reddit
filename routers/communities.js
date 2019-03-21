27afc86d32c2f3f88403d149538280493d462d38
chris.hung <chris.hung@gmail.com>
2019-03-21 18:23:08 +0800

    Remove authentication/authorization code from router

--- routers/communities.js
+++ routers/communities.js
@@ -4,19 +4,11 @@ const postRepository = require('../repositories/postRepository').instance;
 
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
-  if (!req.user) {
-    return res.status(401).json({});
-  }
-  return communityRepository.create(req.body)
-    .then(community => res.status(201).json(community))
-    .catch(error => next(error));
-});
+router.post('/', (req, res, next) => communityRepository.create(req.body)
+  .then(community => res.status(201).json(community))
+  .catch(error => next(error)));
 
 router.post('/:communityName/posts/', (req, res, next) => {
-  if (!req.user) {
-    return res.status(401).json({});
-  }
   const post = Object.assign({ authorId: req.user.userId }, req.body);
   return postRepository.createUnder(req.params.communityName, post)
     .then(posted => res.status(201).json(posted))
