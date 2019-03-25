e324893870063d455d0db6c398f5181d09eaa31d
chris.hung <chris.hung@gmail.com>
2019-03-25 11:30:50 +0800

    Add tests of postRepository.findUnder()

--- test/integretion/repositories/postRepository.test.js
+++ test/integretion/repositories/postRepository.test.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
-const { sequelize, User, Community } = require('../../../orm/models');
+const {
+  sequelize, User, Community, Post,
+} = require('../../../orm/models');
 const postRepository = require('../../../repositories/postRepository').instance;
 const { IdentityNotFoundError } = require('../../../repositories/errors');
 
@@ -13,15 +15,35 @@ const defaultCommunity = {
   communityId: 1,
   communityName: 'comForTest',
 };
+const defaultPosts = [
+  {
+    title: 'test1',
+    content: 'test1 ccontent',
+    authorId: 1,
+    communityId: 1,
+  },
+  {
+    title: 'test2',
+    content: 'test2 ccontent',
+    authorId: 1,
+    communityId: 1,
+  },
+  {
+    title: 'test3',
+    content: 'test3 ccontent',
+    authorId: 1,
+    communityId: 1,
+  },
+];
 // TODO: resolve table lock, when all repository tests run simultaneously.
 describe('postRepository', () => {
   before('Set up user and community data',
     () => sequelize.sync({ force: true })
       .then(() => User.create(defaultUser))
       .then(() => Community.create(defaultCommunity)));
-  afterEach(() => postRepository.sequelizeModel.truncate({ truncate: true }));
 
   describe('creaeteUnder', () => {
+    afterEach(() => postRepository.sequelizeModel.truncate({ truncate: true }));
     it('should work when community and post data are provided', () => {
       const post = {
         title: 'new post',
@@ -57,6 +79,39 @@ describe('postRepository', () => {
         .catch((error) => {
           expect(error).to.be.instanceof(IdentityNotFoundError);
         });
+    });
+  });
+
+  describe('findUnder', () => {
+    before(() => Post.bulkCreate(defaultPosts));
+    it('should use default options when options are undifined', () => postRepository.findUnder(defaultCommunity.communityName)
+      .then((results) => {
+        expect(results).to.exist;
+        expect(results.length).equal(3);
+      }));
+
+    it('should filter out title when options.search is defined', () => {
+      postRepository.findUnder(defaultCommunity.communityName, { search: defaultPosts[0].title })
+        .then((results) => {
+          expect(results).to.exist;
+          expect(results[0].title).to.equal(defaultPosts[0].title);
+        });
+
+      it('should return limited number of posts when options.limit is defined', () => {
+        postRepository.findUnder(defaultCommunity.communityName, { limit: 1 })
+          .then((results) => {
+            expect(results).to.exist;
+            expect(results.length).equal(1);
+          });
+      });
+
+      it('should offset rows when options.offset is defined', () => {
+        postRepository.findUnder(defaultCommunity.communityName, { offset: 1 })
+          .then((results) => {
+            expect(results).to.exist();
+            expect(results.length).to.equal(2);
+          });
+      });
     });
   });
 });
