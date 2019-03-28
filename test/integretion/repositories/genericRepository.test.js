e3a97486adbe7c1163b8cb880341703fdcdf05bb
chris.hung <chris.hung@gmail.com>
2019-03-28 17:10:21 +0800

    Refactor test
    Group set-ups and tests by methods with describe(methodName).
    Extract general test data as defaultModels.

--- test/integretion/repositories/genericRepository.test.js
+++ test/integretion/repositories/genericRepository.test.js
@@ -10,35 +10,39 @@ const defaultModel = {
 
 describe('GenericRepository', () => {
   const repository = new GenericRepositoy(User);
+  before(() => sequelize.sync({ force: true }));
 
-  beforeEach('reset db', () => sequelize.sync({ force: true }));
-
-  it('create',
-    () => repository.create(defaultModel)
-      .then((model) => {
-        expect(model).to.exist;
-      }));
-
-  describe('', () => {
-    beforeEach(() => User.create({
-      userName: 'test',
-      passwordHash: 'tess123',
-      email: 'test@mail.com',
-    }));
-
-    it('findOne', () => repository.findOne({ userName: 'test' })
-      .then((model) => { expect(model).exist; }));
-
-    it('findAll', () => {
-      repository.findAll({ userName: 'test' })
+  describe('create', () => {
+    afterEach(() => User.truncate());
+    it('should create new model',
+      () => repository.create(defaultModel)
         .then((model) => {
-          expect(model).to.instanceOf(Array);
-          expect(model).to.include(defaultModel);
-        });
-    });
+          expect(model).to.exist;
+        }));
+  });
 
-    it('update', () => repository
-      .update({ email: 'updated@eamil.com' }, { userName: 'test' })
+  describe('findOne', () => {
+    before(() => User.create(defaultModel));
+    after(() => User.truncate());
+    it('should find one model', () => repository.findOne({ userName: defaultModel.userName })
+      .then((model) => { expect(model).exist; }));
+  });
+
+  describe('update', () => {
+    before(() => User.create(defaultModel));
+    after(() => User.truncate());
+    it('should update model', () => repository
+      .update({ email: 'updated@eamil.com' }, { userName: defaultModel.userName })
       .then((affectedCount) => { expect(affectedCount).equals(1); }));
+  });
+
+  describe('findAll', () => {
+    before(() => User.create(defaultModel));
+    after(() => User.truncate());
+    it('should find all models', () => repository.findAll({ userName: 'test' })
+      .then((models) => {
+        expect(models).to.instanceOf(Array);
+        expect(models[0]).to.include(defaultModel);
+      }));
   });
 });
